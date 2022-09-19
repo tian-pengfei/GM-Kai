047606e34e1e6e4d9ac701f20086f1e29a6171a1
tianpengfei <tian-pengfei@foxmail.com>
2022-09-19 14:54:44 +0800

    added gmhelper to textHelper for testing

--- gmkai-core/src/test/java/test/TestHelper.java
+++ gmkai-core/src/test/java/test/TestHelper.java
@@ -2,9 +2,12 @@ package test;
 
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.zz.gmhelper.SM2Util;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -85,8 +88,35 @@ public class TestHelper {
 
         Cipher cipher = Cipher.getInstance("SM2", BouncyCastleProvider.PROVIDER_NAME);
         cipher.init(Cipher.ENCRYPT_MODE, ecPublicKey);
-
         return cipher.doFinal(text, 0, text.length);
+    }
+
+    public static byte[] sm2EncryptWithDer(ECPublicKey ecPublicKey, byte[] text) throws Exception {
+        BCECPublicKey bcEcPublicKey = ECPublicKey2BCECPublicKey(ecPublicKey);
+
+        return SM2Util.encodeSM2CipherToDER(SM2Util.encrypt(bcEcPublicKey, text));
+    }
+
+    public static byte[] sm2DecryptWithDer(ECPrivateKey ecPrivateKey, byte[] encryptedText) throws Exception {
+
+        BCECPrivateKey bcecPrivateKey = ECPrivateKey2BCECPrivateKey(ecPrivateKey);
+
+        return SM2Util.decrypt(bcecPrivateKey, SM2Util.decodeDERSM2Cipher(encryptedText));
+    }
+
+
+    static BCECPublicKey ECPublicKey2BCECPublicKey(ECPublicKey ecPublicKey) {
+        if (ecPublicKey instanceof BCECPublicKey) {
+            return (BCECPublicKey) ecPublicKey;
+        }
+        return new BCECPublicKey(ecPublicKey, BouncyCastleProvider.CONFIGURATION);
+    }
+
+    static BCECPrivateKey ECPrivateKey2BCECPrivateKey(ECPrivateKey ecPrivateKey) {
+        if (ecPrivateKey instanceof BCECPrivateKey) {
+            return (BCECPrivateKey) ecPrivateKey;
+        }
+        return new BCECPrivateKey(ecPrivateKey, BouncyCastleProvider.CONFIGURATION);
     }
 
     public static void main(String[] args) {
